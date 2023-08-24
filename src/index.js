db027175414ccecdd661aee8adfb347413edfb27
KatYokai <krsalas@hotmail.com>
2023-08-24 17:23:54 -0400

    Adding time function for multiple locations

--- src/index.js
+++ src/index.js
@@ -22,3 +22,25 @@ function updateTime() {
 
 updateTime();
 setInterval(updateTime, 1000);
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let cityElement = document.querySelector("#cities");
+  cityElement.innerHTML = `
+    <div class="city">
+      <div>
+        <h2>${cityName}</h2>
+        <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+      </div>
+      <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
+    "A"
+  )}</small></div>
+    </div>
+  `;
+}
+
+let citySelection = document.querySelector("#city-selection");
+
+citySelection.addEventListener("change", updateCity);
